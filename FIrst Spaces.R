a8cba0151521b3e13c6841716d4f6a06bd592aeb
Cynthia <cynthianicolevazquez@gmail.com>
2026-07-01 13:38:21 -0700

    Added another plot

--- FIrst Spaces.R
+++ FIrst Spaces.R
@@ -1,6 +1,9 @@
-# My First Scpript in theis project
+# My First Script in this project
 library(ggplot2)
 library(ratdat)
 
-ggplot(complete_old, aes(x=hindfoot_length,y=weight))+
+ggplot(complete_old, aes(x=hindfoot_length,y=weight, color=species))+
+  geom_point()
+
+ggplot(complete_old, aes(x=hindfoot_length,y=weight, color=species))+
   geom_point()
